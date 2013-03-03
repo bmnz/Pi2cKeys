8fc3767861d55119487689e40fdd4e543f9a8ce3
bmnz <bmnz@server.fake>
2013-03-03 16:48:19 -0600

    More progress on Gamepad. Button updated w/r/t Gamepad.

--- Pi2cKeys.py
+++ Pi2cKeys.py
@@ -4,10 +4,17 @@ import uinput
 
 #Kellen Manning 26 FEB 2013
 class Gamepad:
-    "Gamepad class keeps track of button states and emits the appropriate key presses."
-    pass
+    """
+    Gamepad class represents one uinput gamepad device. Button objects are assigned to keys, and this class performs the logic to determine when to send/change keypresses. The gamepad has 8 buttons as designed: A,B,C,START,FORWARD,BACK,LEFT,RIGHT.
 
-    previous_state = 0
+    Methods include:
+    def update_all_the_keys( self ):
+    def update_key( self, button ):
+
+
+    """
+
+    buttons = []
 
     events = (
             #list of keys in /usr/include/linux/input.h
@@ -15,27 +22,59 @@ class Gamepad:
             uinput.BTN_B,
             uinput.BTN_C,
             uinput.BTN_START,
-            uinput.BTN_X, #UP
-            uinput.BTN_Y, #DOWN
-            uinput.BTN_TL, #LEFT
-            uinput.BTN_TR, #RIGHT
+            uinput.BTN_FORWARD, #UP
+            uinput.BTN_BACK, #DOWN
+            uinput.BTN_LEFT, 
+            uinput.BTN_RIGHT, 
             )
     
     device = uinput.Device(events)
 
-    #Emit_Keypress
-    #Logic to Determine whether we call Emit_Keypress
-    #Organize/Contain ALL THE Button objects! Includes logic to assign them.
+    def new_button_append( self, button ):
+        "Append a new button to this Gamepad device. Returns the new number of total buttons in device."
+        self.buttons.append(button)
+        return len(self.buttons)
+
+    #Logic to Determine whether we call device.emit
+    def update_key( self, button ):
+        "Contains logic for using uinput to update state of device.emit. Single button use suggested for debug purposes, use update_all_the_keys() during normal polling use instead."
+        previous = button.get_key_state_previous()
+        current = button.get_key_state()
+
+        if( previous != current ):
+            self.device.emit(button.get_uinput_key(), current) #Arg1 is uinput tuple, Arg2 is boolean state for press/release (1/0)
 
 
+
+    def update_all_the_keys( self ):
+        "Contains logic for using uinput to update state of device.emit for all buttons in gamepad"
+        for b in self.buttons:
+            self.update_key(b)
+#End of Gamepad Class
 
 #Kellen Manning 23 FEB 2013
 class Button:
-    "Button class holds key and button state info, peforms debounce"
+    """
+    Button class holds key and button state info, peforms debounce internally. The following are methods that were intended to be called externally. Please consult the source for internal methods/data.
+    
+    Manipulating Internal State:
+    def set_debounce_max( self, MAXIMUM )
+
+    Gamepad-related Methods:
+    def set_uinput_key( self, uinput_key ):
+    def get_uinput_key( self ):
+    def get_key_state_previous( self ):
+    def get_key_state( self ):
+
+    """
 
     #state variables
     key_state= 0
     button_state = 0
+    
+    #gamepad-related assignments
+    uinput_key = ()
+    key_state_previous = 0
 
     #Debounce constants
     DEBOUNCE_TIME = 0.3
@@ -65,9 +104,9 @@ class Button:
             self.integrator = self.MAXIMUM #"Defensive" code (redundant)
             self.key_state = 1
 
-    def set_button_state( self, button_state ):
+    def set_button_state( self, booly ):
         "Automatically performs debounce upon set"
-        self.button_state = button_state
+        self.button_state = booly
         self.debounce()
 
     def get_button_state( self ):
@@ -75,5 +114,20 @@ class Button:
         return self.button_state
 
     def get_key_state( self ):
+        "Return key's boolean state post-debounce"
+        self.key_state_previous = self.key_state
         return self.key_state
+
+    def get_key_state_previous( self ):
+        "Returns key's boolean state from previous check. Updated from get_key_state, so call this FIRST!"
+        return self.key_state_previous
+
+    def set_uinput_key( self, uinput_key ):
+        "Pass a uinput data tuple such as set_uinput_key( uinput.BTN_START )"
+        self.uinput_key = uinput_key
+
+    def get_uinput_key( self ):
+        "Returns 2-tuple uinput data such as uinput.BTN_START."
+        return self.uinput_key
+
 
